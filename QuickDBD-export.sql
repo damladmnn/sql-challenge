8ac00ec8662c490d78739883d6d96907bed187d1
damladmnn <134240528+damladmnn@users.noreply.github.com>
2023-08-11 00:38:53 -0500

    Add files via upload

---
+++ QuickDBD-export.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Q2pSc0
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" integer   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" integer   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" integer   NOT NULL,
+    "emp_title" varchar   NOT NULL,
+    "id_birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" date   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" integer   NOT NULL,
+    "salary" integer   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "salary"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar   NOT NULL
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_emp" ("");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_emp" ("");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_emp" ("");
+
